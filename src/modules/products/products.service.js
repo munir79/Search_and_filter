5d663395b740a25483be4e7ef99d00c17db6cc88
munir79 <jakirhossainmunir79@gmail.com>
2025-06-25 14:55:51 +0600

    search and filtering

--- src/modules/products/products.service.js
+++ src/modules/products/products.service.js
@@ -10,15 +10,13 @@ const createProductIntoDb = async (productData) => {
 const getAllProductsfromDb = async (query) => {
   let filter = {};
 
-
-//  way -1---------------
+  //  way -1---------------
   //    if (query.search) {
   //   filter.brand = {
   //     $regex: query.search,
   //     $options: "i",
   //   };
   // }
-
 
   //  way--*************************-2********************************
   if (query.search) {
@@ -34,19 +32,33 @@ const getAllProductsfromDb = async (query) => {
 
   // *************************************************dynamic way  ********************************************
 
-//   if(query.search){
-//     const searchFields=["name","brand","category"];
-//     const SearchRegex={
-//       $regex:query.search,
-//       $options:'i'
-//     }
-//  filter.$or = searchFields.map(field => ({
-//       [field]: SearchRegex
-//     }));
+  //   if(query.search){
+  //     const searchFields=["name","brand","category"];
+  //     const SearchRegex={
+  //       $regex:query.search,
+  //       $options:'i'
+  //     }
+  //  filter.$or = searchFields.map(field => ({
+  //       [field]: SearchRegex
+  //     }));
 
-//   }
+  //   }
 
-// *************************************************dynamic way end *********************************
+  // *************************************************dynamic way end *********************************
+
+  // ****************************************************filtering **********************************
+
+  // filter by brand
+  if (query.brand) {
+    filter.brand = query.brand;
+  }
+
+  // filter by category
+  if (query.category) {
+    filter.category = query.category;
+  }
+
+  // filter by price range
 
   const result = await Product.find(filter);
   return result;
